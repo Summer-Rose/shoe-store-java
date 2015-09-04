3cbacf105209ef16eb5e9df3d975ee64e305ccef
Summer <sbrochtrup@gmail.com>
2015-09-04 11:25:41 -0700

    Add route to main index page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,5 +12,15 @@ public class App {
     public static void main(String[] args) {
     	staticFileLocation("/public");
     	String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("stores", Store.all());
+      model.put("brands", Brand.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
     }
 }
