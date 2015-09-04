189f1c774f884d946f10f7027d344b18c3397941
Summer <sbrochtrup@gmail.com>
2015-09-03 17:24:36 -0700

    Add StoreTest file

---
+++ src/test/java/StoreTest.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class StoreTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  
+}
